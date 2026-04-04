0fbbf770044825cb617ebd7c158d466fe595a864
Edmilson Fernandes <edmls2008@gmail.com>
2026-04-04 00:28:43 -0300

    feat(android): add native splash exit animation and Android 12 splash theme

--- mobile/android/app/src/main/java/com/janocaminho/app/MainActivity.java
+++ mobile/android/app/src/main/java/com/janocaminho/app/MainActivity.java
@@ -1,9 +1,18 @@
 package com.janocaminho.app;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
 import android.webkit.CookieManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+
+import androidx.core.splashscreen.SplashScreen;
 
 import com.getcapacitor.BridgeActivity;
 
@@ -13,6 +22,30 @@ public class MainActivity extends BridgeActivity {
     private static final String PREFS_NAME = "jnk_mobile_prefs";
     private static final String LAST_URL_KEY = "last_url";
     private static final String ROOT_DOMAIN = "janocaminho.com.br";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+        splashScreen.setOnExitAnimationListener((provider) -> {
+            View splashView = provider.getView();
+            ObjectAnimator fade = ObjectAnimator.ofFloat(splashView, View.ALPHA, 1f, 0f);
+            ObjectAnimator scaleX = ObjectAnimator.ofFloat(splashView, View.SCALE_X, 1f, 1.04f);
+            ObjectAnimator scaleY = ObjectAnimator.ofFloat(splashView, View.SCALE_Y, 1f, 1.04f);
+
+            AnimatorSet set = new AnimatorSet();
+            set.playTogether(fade, scaleX, scaleY);
+            set.setDuration(360L);
+            set.setInterpolator(new DecelerateInterpolator());
+            set.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    provider.remove();
+                }
+            });
+            set.start();
+        });
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public void onStart() {
